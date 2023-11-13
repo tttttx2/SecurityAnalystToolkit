494da841b5d54d018b8099cc11bc5686970c9dd4
tttttx2 <tttttx2@zeromail.ch>
2023-11-13 12:30:16 +0100

    added reverse dns lookup

--- app/plugins/ip/reverse_dns.py
+++ app/plugins/ip/reverse_dns.py
@@ -1,7 +1,14 @@
 import json
+import dns.resolver,dns.reversename
 
 
 def run(ip):
     response = {'raw':'raw text', 'pretty':'pretty text for gui'}
-    response["pretty"]+=" from reverse_dns"
+
+    try:
+        reverse_dns = '  \n'.join([str(i) for i in dns.resolver.resolve(dns.reversename.from_address(ip), "PTR")])
+    except:
+        reverse_dns = "ERROR: LOOKUP FAILED"
+
+    response["pretty"]="PTR Records:\n  {}".format(reverse_dns)
     return json.dumps(response)
